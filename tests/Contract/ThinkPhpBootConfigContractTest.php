01e63ced523c464a9667cb03a0c75a39546e0f8f
LiaoZhongHao <liao08022040@gmail.com>
2026-09-07 17:49:08 +0800

    fix: add ThinkPHP development router

--- tests/Contract/ThinkPhpBootConfigContractTest.php
+++ tests/Contract/ThinkPhpBootConfigContractTest.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 $root = dirname(__DIR__, 2);
 $cacheConfigPath = $root . '/config/cache.php';
+$routerPath = $root . '/public/router.php';
 
 expectTrue(is_file($cacheConfigPath), 'ThinkPHP cache config must exist so framework services can boot');
 $cache = require $cacheConfigPath;
 expectSame('file', $cache['default'] ?? null, 'ThinkPHP cache default driver must be file');
 expectSame('File', $cache['stores']['file']['type'] ?? null, 'ThinkPHP file cache store must use File driver');
+expectTrue(is_file($routerPath), 'ThinkPHP development server requires public/router.php');
+$router = file_get_contents($routerPath);
+expectTrue(is_string($router) && str_contains($router, "require __DIR__ . '/index.php';"), 'ThinkPHP router must delegate dynamic requests to public/index.php');
