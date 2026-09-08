37376ff2b6d1bf1e6fa062b61e3056cbd4d0f75c
LiaoZhongHao <liao08022040@gmail.com>
2026-09-08 14:02:45 +0800

    test: use repository assertion helpers for R8A provider test

--- tests/Unit/MiniApp/MiniAppProviderAccountTest.php
+++ tests/Unit/MiniApp/MiniAppProviderAccountTest.php
@@ -6,14 +6,14 @@ use app\miniapp\domain\MiniAppConnectionMode;
 use app\miniapp\domain\MiniAppProviderAccount;
 
 $manual = new MiniAppProviderAccount('tenant-1', 'account-1', 'wx-app-1', MiniAppConnectionMode::MANUAL, 'secret-ref-1', null);
-assertSame('tenant-1', $manual->tenantId());
-assertSame('account-1', $manual->accountId());
-assertSame('wx-app-1', $manual->providerAppId());
-assertSame('secret-ref-1', $manual->credentialRef());
-assertSame(null, $manual->componentPlatformId());
+expectSame('tenant-1', $manual->tenantId());
+expectSame('account-1', $manual->accountId());
+expectSame('wx-app-1', $manual->providerAppId());
+expectSame('secret-ref-1', $manual->credentialRef());
+expectSame(null, $manual->componentPlatformId());
 
 $component = new MiniAppProviderAccount('tenant-1', 'account-2', 'wx-app-2', MiniAppConnectionMode::COMPONENT, null, 'component-1');
-assertSame('component-1', $component->componentPlatformId());
+expectSame('component-1', $component->componentPlatformId());
 
 assertThrows(
     fn () => new MiniAppProviderAccount('tenant-1', 'account-1', 'wx-app-1', MiniAppConnectionMode::MANUAL, null, null),
