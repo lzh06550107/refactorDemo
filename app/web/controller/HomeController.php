5b90bff31cf6455d58ce4231c5c1bf167d121795
LiaoZhongHao <liao08022040@gmail.com>
2026-09-11 13:05:52 +0800

    feat: map missing web theme page to safe html 404

--- app/web/controller/HomeController.php
+++ app/web/controller/HomeController.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace app\web\controller;
 
 use app\web\support\WebAssetManifest;
+use app\web\support\WebErrorPage;
 use modules\theme\application\RenderThemePage;
+use modules\theme\domain\ThemePageNotFound;
 use think\Response;
 
 final class HomeController
@@ -19,13 +21,23 @@ final class HomeController
     public function index(): Response
     {
         $assets = $this->assets->forEntry('src/js/main.js');
-        $html = $this->renderThemePage->execute('corporate', 'index', [
+        $viewModel = [
             'site_title' => 'WePlatform',
             'page_title' => 'WePlatform',
             'page_description' => 'ThinkPHP 8 Web/H5 theme runtime',
             'asset_css_url' => $assets['css'],
             'asset_js_url' => $assets['js'],
-        ]);
+        ];
+
+        try {
+            $html = $this->renderThemePage->execute('corporate', 'index', $viewModel);
+        } catch (ThemePageNotFound) {
+            return response(
+                WebErrorPage::notFound(),
+                404,
+                ['Content-Type' => 'text/html; charset=UTF-8'],
+            );
+        }
 
         return response($html, 200, ['Content-Type' => 'text/html; charset=UTF-8']);
     }
