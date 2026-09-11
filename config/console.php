4f87c1aac039ab709168ae09844d097ace6b6a7e
LiaoZhongHao <liao08022040@gmail.com>
2026-09-11 16:53:56 +0800

    feat: register admin bootstrap command

--- config/console.php
+++ config/console.php
@@ -2,10 +2,12 @@
 
 declare(strict_types=1);
 
+use app\worker\command\AdminBootstrapCommand;
 use app\worker\command\OpenPlatformProvisioningWorkerCommand;
 
 return [
     'commands' => [
+        AdminBootstrapCommand::class,
         OpenPlatformProvisioningWorkerCommand::class,
     ],
 ];
