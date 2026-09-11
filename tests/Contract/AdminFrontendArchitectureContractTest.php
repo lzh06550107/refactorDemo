bdf40e777ededf8dd29cdef3c225ac8e891fbfa7
LiaoZhongHao <liao08022040@gmail.com>
2026-09-11 16:51:14 +0800

    test: execute admin bootstrap repository and command wiring contract

--- tests/Contract/AdminFrontendArchitectureContractTest.php
+++ tests/Contract/AdminFrontendArchitectureContractTest.php
@@ -88,4 +88,5 @@ declare(strict_types=1);
 
 require dirname(__DIR__) . '/Component/AdminUi/SpaControllerTest.php';
 require dirname(__DIR__) . '/Component/Iam/BootstrapFirstAdminTest.php';
+require dirname(__DIR__) . '/Component/Iam/ThinkPhpBootstrapAdminRepositoryTest.php';
 require __DIR__ . '/AdminFoundationCompletionContractTest.php';
