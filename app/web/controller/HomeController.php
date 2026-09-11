746e2c422c1de38813727e432b94359b0b074ac3
LiaoZhongHao <liao08022040@gmail.com>
2026-09-11 12:59:28 +0800

    feat: add first server-rendered web home controller

---
+++ app/web/controller/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\web\controller;
+
+use app\web\support\WebAssetManifest;
+use modules\theme\application\RenderThemePage;
+use think\Response;
+
+final class HomeController
+{
+    public function __construct(
+        private readonly RenderThemePage $renderThemePage,
+        private readonly WebAssetManifest $assets,
+    ) {
+    }
+
+    public function index(): Response
+    {
+        $assets = $this->assets->forEntry('src/js/main.js');
+        $html = $this->renderThemePage->execute('corporate', 'index', [
+            'site_title' => 'WePlatform',
+            'page_title' => 'WePlatform',
+            'page_description' => 'ThinkPHP 8 Web/H5 theme runtime',
+            'asset_css_url' => $assets['css'],
+            'asset_js_url' => $assets['js'],
+        ]);
+
+        return response($html, 200, ['Content-Type' => 'text/html; charset=UTF-8']);
+    }
+}
